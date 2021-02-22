bb5d194aeb78ea84b830b8d5a6f919ac19ed156f
JRazek <jakub.jakubecky123@gmail.com>
2021-02-22 23:28:37 +0100

    changes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include "neuralNetwork/Net.h"
-#include "neuralNetwork/netStructure/FFLayer/FFLayer.h"
 
 int main(){
     Net n = Net({{0,1}, {0,2}});
-   // n.shoutAtMePls();
-   
 }
