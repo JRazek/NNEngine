51ff1d7c0fc87a947ced5fb43478470dadd54334
JRazek <jakub.jakubecky123@gmail.com>
2021-02-27 15:43:25 +0100

    not compiling

--- neuralNetwork/netStructure/ConvolutionLayer/CLayer.cpp
+++ neuralNetwork/netStructure/ConvolutionLayer/CLayer.cpp
@@ -7,3 +7,17 @@ void CLayer::run(const std::vector<float> &input){
     std::cout<<"Im a fucking CLayer!\n";
 }
 CLayer::~CLayer(){}
+
+CLayer::Tensor::Tensor(int x, int y, int z):matrices(z){
+    for(int i = 0; i < z; i ++){
+        matrices.push_back(Matrix(x, y));
+    }
+}
+CLayer::Tensor::Matrix::Matrix(int sizeX, int sizeY){
+    for(int y = 0; y < sizeY; y++){
+        weights.push_back(std::vector<float>());
+        for(int x = 0; x < sizeX; x++){
+            weights[y].push_back(0);
+        }
+    }
+}
--- neuralNetwork/netStructure/ConvolutionLayer/CLayer.h
+++ neuralNetwork/netStructure/ConvolutionLayer/CLayer.h
@@ -1,9 +1,16 @@
 #pragma once
-#include "CLayer.h"
 #include <netStructure/Layer/Layer.h>
 
 struct CLayer : Layer{
     CLayer(int id, Net * net, int kernelSizeX, int kernelSizeY);
     void run(const std::vector<float> &input);
     ~CLayer();
+    struct Tensor{
+        struct Matrix{
+            Matrix(int sizeX, int sizeY);
+            std::vector< std::vector< float > > weights;
+        };
+        std::vector<Matrix> matrices;
+        Tensor(int x, int y, int z);
+    };
 };
