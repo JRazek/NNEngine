3c595d34ea835ecd686b11d2080d2aa79aa58170
JRazek <jakub.jakubecky123@gmail.com>
2021-03-02 00:25:06 +0100

    last commit

--- neuralAlgorithms/geneticLearning/GeneticLearningUnit.cpp
+++ neuralAlgorithms/geneticLearning/GeneticLearningUnit.cpp
@@ -1,1 +1,10 @@
 #include "GeneticLearningUnit.h"
+GeneticLearningUnit::GeneticLearningUnit(const std::vector< std::vector < float> > structure, int goalGenerations, int individualsPerGeneration):structure(structure){
+    this->goalGenerations = goalGenerations;
+    this->individualsPerGeneration = individualsPerGeneration;
+}
+void GeneticLearningUnit::start(){
+    for(int i = 0; i < this->individualsPerGeneration; i ++){
+        
+    }
+}
--- neuralAlgorithms/geneticLearning/GeneticLearningUnit.h
+++ neuralAlgorithms/geneticLearning/GeneticLearningUnit.h
@@ -1,4 +1,14 @@
 #pragma once
-class LearningUnit{
-    
+#include <vector>
+#include <neuralNetwork/Net.h>
+class GeneticLearningUnit{
+    std::vector<Net> currIndividuals;
+public:
+    const std::vector< std::vector < float> > structure ;
+    int generationNum = 0;
+
+    int goalGenerations;
+    int individualsPerGeneration;
+    GeneticLearningUnit(const std::vector< std::vector < float> > structure, int goalGenerations, int individualsPerGeneration);
+    void start();
 };
