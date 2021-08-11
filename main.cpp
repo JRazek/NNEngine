d53c190d9453fab731b07fad49f38b0f229fb090
JRazek <jakub.jakubecky123@gmail.com>
2021-08-12 00:35:13 +0200

    test generator done.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 #include "Network/Network.h"
 #include "Network/layers/ConvolutionLayer.h"
 #include <opencv2/opencv.hpp>
+#include <fstream>
+
 int main(){
     cv::Mat mat = cv::imread("resources/aPhoto.jpg");
     cn::Network network(800, 800, 3);
@@ -25,23 +27,39 @@ int main(){
 //    PointData pointData6({4,7}, &t);
 //    std::vector<PointData *> points = {&pointData1, &pointData2, &pointData3, &pointData4, &pointData5, &pointData6};
 
+    int pointsCount, testsCount;
+    std::cin >> pointsCount >> testsCount;
 
     std::vector<PointData *> points;
-    for(int i = 0; i < 1e5; i ++){
-        int randX = rand() % 10000;
-        int randY = rand() % 10000;
-        points.push_back(new PointData({randX, randY}));
+    points.reserve(pointsCount);
+
+    std::vector<std::pair<std::pair<float, float>, std::pair<float, float>>> tests;//test, answer
+    tests.reserve(pointsCount);
+    for(int i = 0; i < pointsCount; i ++){
+        float x,y;
+        std::cin >> x >> y;
+        points.push_back(new PointData({x, y}));
     }
-    KDTree kdTree (points);
-    for(int i = 0; i < 1e4; i ++) {
-        int randX = rand() % 10000;
-        int randY = rand() % 10000;
-        auto result = kdTree.findNearestNeighbour({randX, randY});
-        std::cout << "x: " << result.first->point.first << " y: " << result.first->point.second<<"\n";
+    for(int i = 0; i < testsCount; i ++){
+        float xt,yt,xa,ya;
+        std::cin >> xt >> yt >> xa >> ya;
+        tests.push_back({{xt, yt}, {xa, ya}});
     }
 
+    KDTree kdTree(points);
+
+    for(auto p : tests){
+        auto res = kdTree.findNearestNeighbour(p.first);
+        if(res.first->point == p.second){
+            std::cout<<"OK \n";
+        }else{
+            std::cout<<"WRONG \n";
+        }
+    }
     for(auto p : points){
         delete p;
+    }
+    for(auto p : tests){
     }
     //std::pair<int, int> neighbor = quadTree.getNearestNeighbour({4, 4});
     return 0;
--- testGenerator.cpp
+++ testGenerator.cpp
@@ -2,34 +2,59 @@
 using namespace  std;
 
 pair<float, float> nearestNeighbor(const pair<float, float> &point, vector<pair<float, float>> &points){
-
+    float best = INFINITY;
+    pair<float, float> bestPair;
+    for(auto p : points){
+        float res = pow(point.first - p.first, 2) + pow(point.second - p.second, 2);
+        if(res < best){
+            best = res;
+            bestPair = p;
+        }
+    }
+    return bestPair;
 }
 
 int main() {
     int seed = 0;
     srand(seed);
 
-    const int pointsCount = 1e4;
-    const int testsCount = 1e4;
+    const int pointsCount = 10;
+    const int testsCount = 10;
+
+
+    const int maxX = 10, maxY = 10;
 
     vector<pair<float, float>> points(pointsCount);
 
     for(int i = 0; i < pointsCount; i ++){
-        points[i] = {rand() % 10000, rand() % 10000};
+        points[i] = {rand() % maxX, rand() % maxY};
     }
 
     vector<pair<float, float>> tests(testsCount);
     for(int i = 0; i < testsCount; i ++){
-        tests[i] = {rand() % 10000, rand() % 10000};
+        tests[i] = {rand() % maxX, rand() % maxY};
     }
 
     vector<pair<float, float>> answers(testsCount);
 
     for(int i = 0; i < testsCount; i ++){
-        pair<float, float> ans = nearestNeighbor(answers[i], points);
+        pair<float, float> ans = nearestNeighbor(tests[i], points);
+        answers[i] = ans;
     }
 
+    fstream outputFile("test.in", ios::out);
 
+    outputFile << pointsCount << "\n";
+    outputFile << testsCount << "\n";
+    for(auto p : points){
+        outputFile << p.first << " " << p.second << "\n";
+    }
+
+    for(int i = 0; i < testsCount; i ++){
+        auto p = tests[i];
+        auto ans = answers[i];
+        outputFile << p.first << " " << p.second << " " << ans.first << " " << ans.second <<  "\n";
+    }
 
     return 0;
 }
